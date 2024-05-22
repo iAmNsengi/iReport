3202903e0127b86c5dd74bde9b0d7e42ae389b07
Eliezer Nsengi <avaranstudios@gmail.com>
2024-05-22 17:03:37 +0200

    update: create models

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    current_class = models.ManyToManyField('TheClass')
-    dob = models.CharField(max_length=20)
+    current_class = models.ForeignKey('TheClass',on_delete=models.CASCADE)
     
 
     def __str__(self):
@@ -13,6 +14,7 @@ class Student(models.Model):
 
 
 class TheClass(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
     code = models.CharField(max_length=5)
     courses = models.ManyToManyField('Course')
@@ -21,6 +23,14 @@ class TheClass(models.Model):
         return self.name + "  - " + self.code
 
 class Course(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=10)
     code = models.CharField(max_length=5)
 
+
+class Data(models.Model):
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    cat1 = models.FloatField()
+    cat2 =models.FloatField()
+    fat = models.FloatField()
+    total = models.FloatField()
